78f8410a6b6d972d51d0815d84dec052a30d34f6
Yordan Pavlov <y.d.pavlov@gmail.com>
2018-05-25 13:20:44 +0300

    Roulette compiling, methods extended

--- contracts/Roulette.sol
+++ contracts/Roulette.sol
@@ -3,27 +3,21 @@ pragma solidity ^0.4.19;
 
 contract Lottery {
 
-  enum Position {
-      zero, one, two, three, four, five, six, seven, eight, nine, ten,
-      eleven, twelve, thirteen, fourteen, fifthteen, sixteen, nineteen, twenty,
-      twentyone, twentytwo, twentythree, twentyfour, twentyfive, twentysix, twentyseven, twentyeight, twenty nine,
-      thirty, thirtyone, thirtytwo, thirtythree, thirtyfour, thirtyfive, thirtysix
-  }
-
   struct Bet {
-      Position position;
+      uint8 position;
       uint value;
   }
 
   struct BetsUser {
-    uint placementTime = 0; // height of blockchain at the time of placement
-    Bet[49] bets;
-    uint8 betsLength = 0;
+    uint placementTime; // height of blockchain at the time of placement
+    Bet[] bets;
+    uint8 betsLength;
   }
 
+  address owner;
   mapping (address => BetsUser) betsPerUser;
   mapping (address => uint) depositPerUser;
-  bytes32[] public listHashes;
+
   // We are not going to clear the list of hashes so we need the 'real' size
   //uint public listHashesCurrentSize = 0;
 
@@ -31,18 +25,20 @@ contract Lottery {
     owner = msg.sender;
   }
 
-  function getAddressOwner(bytes32 hash) public view returns (address){
-    return hashBetToOwner[hash].beneficiary;
-  }
-
-  function placeBet(uint8 position) public {
+  function placeBet(uint8 position) public payable {
       require(0 == betsPerUser[msg.sender].placementTime);
       require(position <= 49);
       require(msg.value <= 1 ether);
-      Bet newBet = new Bet();
-      newBet.position = position;
-      newBet.value = msg.value;
-      betsPerUser[msg.sender].bets[position] = newBet;
+      Bet memory newBet = Bet(position, msg.value);
+      //newBet.position = position;
+      //newBet.value = msg.value;
+      if(betsPerUser[msg.sender].bets.length > betsPerUser[msg.sender].betsLength) {
+        // The array has been extended enough before.
+          betsPerUser[msg.sender].bets[betsPerUser[msg.sender].betsLength] = newBet;
+      } else {
+        betsPerUser[msg.sender].bets.push(newBet);
+      }
+      ++betsPerUser[msg.sender].betsLength;
   }
 
   function finalizeBets() public {
@@ -55,19 +51,26 @@ contract Lottery {
     betsPerUser[msg.sender].betsLength = 0;
   }
 
-  function claimBet() public {
-    require(betsPerUser[msg.sender].placementTime > 0);
-    require(betsPerUser[msg.sender].placementTime <= block.number - 2);
+  function claimBets() public {
+    uint placementTime = betsPerUser[msg.sender].placementTime;
+    require(placementTime > 0 && placementTime <= block.number - 2);
+
     /* Make sure the hash is still visible
      *
      * "You can only access the hashes of the most recent 256 blocks, all other values will be zero."
      */
-    uint blockHashRandomness = blockhash(betsPerUser[msg.sender].placementTime + 2);
+    uint blockHashRandomness = uint(block.blockhash(placementTime + 2));
     require(blockHashRandomness > 0);
 
-    uint8 chosenNumber = blockHashRandomness % 37;
+    uint chosenNumber = blockHashRandomness % 37;
 
-    //TODO
+    for(uint8 index = 0; index < betsPerUser[msg.sender].betsLength; ++index) {
+      if(betsPerUser[msg.sender].bets[index].position == chosenNumber) {
+        // Win on exact number
+        msg.sender.transfer(36 * betsPerUser[msg.sender].bets[index].value);
+      }
+      // TODO add other bet places
+    }
   }
 
   function deposit() public payable {
